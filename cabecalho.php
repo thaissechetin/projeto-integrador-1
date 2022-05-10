ac0df087ac5a6395f405de332aad085c430835a4
Thaís Sechetin <93165270+thaissechetin@users.noreply.github.com>
2022-05-10 14:20:52 -0300

    comentarios

--- cabecalho.php
+++ cabecalho.php
@@ -63,24 +63,3 @@
         
     </ul>
 </nav>
-    <main id="container">
-        <section>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, temporibus necessitatibus asperiores unde aliquam ex vitae blanditiis saepe amet molestiae eveniet dolorum illo voluptate accusamus id culpa, doloribus exercitationem aperiam!</p>
-            
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime excepturi, quaerat soluta reprehenderit ex consequuntur culpa totam. Aliquid alias magnam cum tempore facere est molestiae quis, optio adipisci illum! Assumenda?</p>
-
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita eos, repellendus autem laboriosam corrupti totam quia reiciendis laborum architecto voluptatibus quisquam asperiores velit ipsum, eveniet, veniam maiores sit aliquam amet?</p>
-
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat odio enim facilis eaque voluptatem perspiciatis nisi ab et fuga minima nesciunt veritatis ea, beatae aliquid aspernatur eos amet laudantium rem!</p>
-
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio sequi nisi reprehenderit tempore. Saepe esse natus deleniti hic nobis. Iusto minus quaerat autem, in maiores quidem! Hic est vero quia.</p>
-
-
-        </section>
-    </main>
-
-    <footer>
-        <p class="do-footer">Desenvolvido por alunos Senac 2022</p>
-    </footer>
-</body>
-</html>
